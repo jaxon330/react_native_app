a13a0e9bfadafcaca02dbdd960e71f007124b7a1
john-330 <jaxon330@gmail.com>
2023-04-28 23:29:13 -0500

    New commit

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
 import { Link, Stack } from 'expo-router';
 
+import { COLORS, icons, images, SIZES } from '../constants';
 const Home = () => {
     return (
         <View>
